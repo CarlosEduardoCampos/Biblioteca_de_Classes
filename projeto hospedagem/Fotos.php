f7322ca4f5f24eb65442f043bb7ac9e18d3b8747
Carlos <carlos.admcode@gmail.com>
2021-10-05 08:32:11 -0300

    Cria classes e formularios

---
+++ projeto hospedagem/Fotos.php
@@ -0,0 +1,60 @@
+<?php 
+    class Fotos
+    {
+        //Atributos
+        private $txtNomeFoto;
+
+        //Objeto
+        private $fkQuarto;
+
+        /**
+         * Get the value of txtNomeFoto
+         */ 
+        public function getTxtNomeFoto()
+        {
+            return $this->txtNomeFoto;
+        }
+
+        /**
+         * Set the value of txtNomeFoto
+         *
+         * @return  self
+         */ 
+        public function setTxtNomeFoto($txtNomeFoto)
+        {
+            $this->txtNomeFoto = $txtNomeFoto;
+
+            return $this;
+        }
+
+        /**
+         * Get the value of fkQuarto
+         */ 
+        public function getFkQuarto()
+        {
+            return $this->fkQuarto;
+        }
+
+        /**
+         * Set the value of fkQuarto
+         *
+         * @return  self
+         */ 
+        public function setFkQuarto($fkQuarto)
+        {
+            $this->fkQuarto = $fkQuarto;
+
+            return $this;
+        }
+
+        /**
+         * Recebe um arquivo eo salva
+        */
+        public function Upload($files)
+        {
+            $filename = $files["txtNomeArquivo"]["name"];
+            move_uploaded_file($files["txtNomeArquivo"]["tmp_name"],"fotos_quartos/$filename");
+        }//fim função upload
+        
+    }//fim class
+?>
