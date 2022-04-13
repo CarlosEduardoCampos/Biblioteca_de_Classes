90c8cadff861b6cade3348e3cf7ee07aca17ac2b
Carlos <carlos.admcode@gmail.com>
2022-04-13 14:41:05 -0300

    Desenvolve formulário para cadastro de noticias

--- NodeJS/portal_noticias/app/routes/form_save_noticia.js
+++ NodeJS/portal_noticias/app/routes/form_save_noticia.js
@@ -4,4 +4,11 @@ module.exports = function(app)
     {
         res.render("admin/form_add_noticia");
     });
+
+    app.post('/noticia_salvar', function (req, res)
+    {
+        let noticia = req.body;
+        res.send(noticia);
+        //res.render("admin/form_add_noticia");
+    });
 }
