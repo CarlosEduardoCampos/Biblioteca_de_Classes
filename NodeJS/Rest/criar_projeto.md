1af0657b65f1515aff7f6dbef9e34508ad75b865
Carlos <carlos.admcode@gmail.com>
2022-05-17 13:25:04 -0300

    Praticando criar projetos em react

--- NodeJS/Rest/criar_projeto.md
+++ NodeJS/Rest/criar_projeto.md
@@ -1,5 +1,10 @@
 # Iniciar projeto
-    -> npx create-react-app cursoreact
-    
+    -> npx create-react-app (nome do diretorio)
+
+# Simulador de API
+    -> npm install json-server
+
+# Biblioteca de icones 
+    -> npm install react-icons
 # Rodar servidor
     -> npm start
--- NodeJS/Rest/cursoreact/src/App.js
+++ NodeJS/Rest/cursoreact/src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
 import AnotherComponent from './components/AnotherComponent';
 import FirstComponent from './components/FirstComponent';
+//import Images from './components/Images';
+import Hooks from './components/Hooks';
+import List from './components/List';
+import RenderCound from './components/RenderCond';
+import Fragment from './components/Fragment';
+import Container from './components/Container';
 
 function App() {
   return (
@@ -8,6 +14,14 @@ function App() {
       <h2> Hello React </h2>
       <FirstComponent/>
       <AnotherComponent/>
+      <Hooks/>
+      <List/>
+      <RenderCound x={8} y = {10}/>
+      <Container>
+        <h1> Este é Filho do container!</h1>
+        <Fragment/>
+      </Container>
+      
     </div>
   );
 }
